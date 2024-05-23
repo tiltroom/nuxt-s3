fb81b0d252c690948e888ae57c9c4a63d7420984
tiltroom <dragilie@protonmail.com>
2024-05-23 18:02:27 +0300

    up

--- src/runtime/server/api/mutation/create.ts
+++ src/runtime/server/api/mutation/create.ts
@@ -1,40 +1,52 @@
-import { readMultipartFormData, createError, defineEventHandler } from 'h3'
-import { normalizeKey, getKey, getMeta, verifySize, verifyType } from '../../utils'
-import type { PrivateConfig } from '../../../types'
+import { readMultipartFormData, createError, defineEventHandler } from "h3";
+import {
+  normalizeKey,
+  getKey,
+  getMeta,
+  verifySize,
+  verifyType,
+} from "../../utils";
+import type { PrivateConfig } from "../../../types";
 // @ts-ignore
-import { useRuntimeConfig, useStorage } from '#imports'
+import { useRuntimeConfig, useStorage } from "#imports";
 
 export default defineEventHandler(async (event) => {
-  const privateConfig = useRuntimeConfig().s3 as PrivateConfig
+  const privateConfig = useRuntimeConfig().s3 as PrivateConfig;
 
-  const key = getKey(event)
+  const key = getKey(event);
 
-  const multipartFormData = await readMultipartFormData(event)
+  console.log(key);
 
-  const file = multipartFormData?.find(el => el.name === 'file')
+  const multipartFormData = await readMultipartFormData(event);
+
+  console.log(multipartFormData);
+
+  const file = multipartFormData?.find((el) => el.name === "file");
 
   if (file) {
-    const normalizedKey = normalizeKey(key)
+    const normalizedKey = normalizeKey(key);
 
-    verifyType(file.type)
-    verifySize(file.data.length)
+    verifyType(file.type);
+    verifySize(file.data.length);
 
-    const meta = await getMeta(event)
+    const meta = await getMeta(event);
 
-    await useStorage('s3').setItemRaw(normalizedKey, file.data, {
+    console.log("Before setItem");
+
+    await useStorage("s3").setItemRaw(normalizedKey, file.data, {
       meta,
       headers: {
-        'Content-Type': file.type,
-        'Content-Length': file.data.length
-      }
-    })
+        "Content-Type": file.type,
+        "Content-Length": file.data.length,
+      },
+    });
 
-    if (privateConfig.driver === 'fs') {
-      await useStorage('s3').setMeta(normalizedKey, meta)
+    if (privateConfig.driver === "fs") {
+      await useStorage("s3").setMeta(normalizedKey, meta);
     }
 
-    return { status: 'ok' }
+    return { status: "ok" };
   }
 
-  throw createError('invalid-file')
-})
+  throw createError("invalid-file");
+});
